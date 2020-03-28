9856dd8cd28590a94e0eb5cc516effbb4bd65885
oriluka <mariahtato@gmail.com>
2020-03-28 10:43:23 -0700

    Added port num

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // const cors = require('cors')
 const path = require('path')
-const port
+const port = 3400
 // const port = process.env.PORT || 3400;
 
 let app = express();
